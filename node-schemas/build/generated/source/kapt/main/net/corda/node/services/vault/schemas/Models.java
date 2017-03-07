1fce2296a2917cca8020c7b20e43acc436b77828
josecoll <jose.coll@r3cev.com>
2017-03-07 16:37:12 +0000

    Updated generated schema code with Requery 1.2.0

--- node-schemas/build/generated/source/kapt/main/net/corda/node/services/vault/schemas/Models.java
+++ node-schemas/build/generated/source/kapt/main/net/corda/node/services/vault/schemas/Models.java
@@ -8,9 +8,9 @@ import javax.annotation.Generated;
 @Generated("io.requery.processor.EntityProcessor")
 public class Models {
     public static final EntityModel VAULT = new EntityModelBuilder("vault")
-    .addType(VaultTxnNoteEntity.$TYPE)
-    .addType(VaultCashBalancesEntity.$TYPE)
     .addType(VaultStatesEntity.$TYPE)
+    .addType(VaultCashBalancesEntity.$TYPE)
+    .addType(VaultTxnNoteEntity.$TYPE)
     .build();
 
     private Models() {
